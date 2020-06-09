8ea3fa069cc3ad0832684a43fdbdf2d642ab6d0e
youyou-zero <zt_zero@126.com>
2020-06-10 05:42:24 +0800

    test1.java修改一行代码

---
+++ src/main/test/test1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class test1 {
+    @Test
+    public void test(){
+        System.out.println("little cute");
+        System.out.println("second:color");
+
+
+    }
+
+}
