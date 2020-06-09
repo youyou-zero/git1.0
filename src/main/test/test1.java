1b91d16b002668337e483bc3b0c6421286c6c9f1
youyou-zero <zt_zero@126.com>
2020-06-10 06:36:33 +0800

    "另一个故事： snow"

--- src/main/test/test1.java
+++ src/main/test/test1.java
@@ -6,6 +6,8 @@ public class test1 {
         System.out.println("little cute");
         System.out.println("second:color");
         System.out.println("海的女儿");
+        System.out.println("白雪公主");
+
 
 
 
