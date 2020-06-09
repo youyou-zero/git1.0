48bf00e77c6b3ecc3664372bfacd5e66c8e7e76d
youyou-zero <zt_zero@126.com>
2020-06-10 07:02:21 +0800

    "本地接受到远程文件"

--- src/main/test/test2.java
+++ src/main/test/test2.java
@@ -1,6 +1,10 @@
+import org.junit.Test;
+
 public class test2{
 @Test
 public void test1(){
   System.out.println("do something iat remote");
+  System.out.println("at local receive: import some");
+
 }
 }
